f323e837d9924ca179ea7805a3528b52a7a19e0b
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2023-01-22 00:26:23 -0300

    Using events to add location in header

--- src/main/java/br/com/eliarlan/emoney/api/controller/CategoryController.java
+++ src/main/java/br/com/eliarlan/emoney/api/controller/CategoryController.java
@@ -2,9 +2,11 @@ package br.com.eliarlan.emoney.api.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.eliarlan.emoney.api.event.ResourceCreatedEvent;
 import br.com.eliarlan.emoney.domain.model.Category;
 import br.com.eliarlan.emoney.domain.service.CategoryService;
 
@@ -23,14 +26,18 @@ public class CategoryController {
 	@Autowired
 	private CategoryService sevice;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Category> getAll(){
 		return sevice.getAll();
 	}
 	
 	@PostMapping
-	public ResponseEntity<Category> addCategory(@Valid @RequestBody Category category) {
+	public ResponseEntity<Category> addCategory(@Valid @RequestBody Category category, HttpServletResponse response) {
 		Category savedCategory = sevice.save(category);
+		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedCategory.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
 	}
 }
