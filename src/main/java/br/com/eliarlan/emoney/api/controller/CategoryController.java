ed4dc8fdb37d55ffb4adbaed0cf43434d7290757
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2023-01-22 18:11:00 -0300

    Create method deleteById

--- src/main/java/br/com/eliarlan/emoney/api/controller/CategoryController.java
+++ src/main/java/br/com/eliarlan/emoney/api/controller/CategoryController.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.eliarlan.emoney.api.event.ResourceCreatedEvent;
@@ -48,5 +50,11 @@ public class CategoryController {
 		Category savedCategory = service.save(category);
 		publisher.publishEvent(new ResourceCreatedEvent(this, response, savedCategory.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) {
+		service.deleteById(id);
 	}
 }
--- src/main/java/br/com/eliarlan/emoney/domain/service/CategoryService.java
+++ src/main/java/br/com/eliarlan/emoney/domain/service/CategoryService.java
@@ -29,5 +29,9 @@ public class CategoryService {
 	public Optional<Category> findById(Long id) {
 		return repository.findById(id);
 	}
+
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
 	
 }
