17dc9a0913189fe3c92c5bdb09d3c41ec02f342d
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2023-01-21 22:15:37 -0300

    first commit

---
+++ src/main/java/br/com/eliarlan/emoney/EmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.eliarlan.emoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eliarlan/emoney/EmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eliarlan.emoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
